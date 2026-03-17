648c0adf7b6fac6a559aac65ccbc235ce93c383e
Paweł Wieczorek <pawiecz@collabora.com>
2026-03-17 11:44:10 +0100

    client: Add missing Vite plugin configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,15 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    tsconfigPaths(),
+    commonjs(),
   ],
   resolve: {
     alias: {
