5fd6073e643f7035351fdc78971fd86095f32bf8
Juan S. Mrad <juansmrad@gmail.com>
2026-04-20 23:05:10 -0500

    fix ci

--- server/routes/integration_logos/serveIntegrationLogoWithBackground.ts
+++ server/routes/integration_logos/serveIntegrationLogoWithBackground.ts
@@ -11,7 +11,9 @@ export default function serveIntegrationLogoWithBackground(
   _deps: Dependencies,
 ): RequestHandlerWithBodies<Record<string, never>, undefined> {
   return (req, res, next) => {
-    const integrationId = req.params['integrationId'];
+    const rawIntegrationId = req.params['integrationId'];
+    const integrationId =
+      typeof rawIntegrationId === 'string' ? rawIntegrationId : undefined;
     if (!integrationId || integrationId.length === 0) {
       return next(
         makeNotFoundError('Missing integration id.', { shouldErrorSpan: true }),
