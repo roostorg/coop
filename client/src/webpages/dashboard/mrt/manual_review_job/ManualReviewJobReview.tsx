0b5af004fa74c5e9f9c23e95125f235139e44714
Juan S. Mrad <juansmrad@gmail.com>
2026-05-14 20:04:37 -0500

    [fix] Display report information on other reports table

--- client/src/webpages/dashboard/mrt/manual_review_job/ManualReviewJobReview.tsx
+++ client/src/webpages/dashboard/mrt/manual_review_job/ManualReviewJobReview.tsx
@@ -831,7 +831,7 @@ function ManualReviewJobReviewImpl(props: {
   const otherReports =
     reportHistory.length > 1 ? (
       <MergedReportsComponent
-        primaryReportedAt={job.createdAt}
+        primaryReportId={reportHistory[0]?.reportId}
         reportHistory={reportHistory}
       />
     ) : null;
